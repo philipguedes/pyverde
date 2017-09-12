a67c4089fd7d0f7c5f3acf33a3fc9165cdf3c8d5
Philip Guedes <philipguedes@gmail.com>
2017-09-12 11:39:39 -0300

    Improve get_url to cover more cases

--- pyverde/utils.py
+++ pyverde/utils.py
@@ -1,13 +1,16 @@
 import os
-
-# 813140
-# https://br1.api.riotgames.com/lol/summoner/v3/summoners/by-name/czekanie%20masz?api_key=RGAPI-a196b74e-586d-4154-bdd9-f3881e4baa18
+import urllib.parse as parse
+from urllib.parse import urlparse, parse_qsl, urlencode
 
 
 REGION = os.getenv('API_REGION', 'br1')
-API_KEY = os.getenv('API_KEY', 'RGAPI-a196b74e-586d-4154-bdd9-f3881e4baa18')
+API_KEY = os.getenv('API_KEY', '')
 
 
 def get_url(path):
-    url = "https://{}.api.riotgames.com{}?api_key={}".format(REGION, path, API_KEY)
-    return url
+    base_url = "https://{}.api.riotgames.com".format(REGION)
+    parsed_url = urlparse(path)
+    query = dict(parse_qsl(parsed_url.query))
+    query.update({'api_key': API_KEY})
+    encoded_query = urlencode(query)
+    return "{}{}?{}".format(base_url, parsed_url.path, encoded_query)
